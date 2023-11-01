c5ce7385d05bf831cd43f8751f46bf3d92b0eb87
MartinPettersen <searchingtheskyfor@gmail.com>
2023-11-01 13:14:50 +0100

    change header layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,47 +33,48 @@ const Header = () => {
   return (
     <header className="flex flex-col items-center  ">
       <TopHeader />
-      <Link href="/">
-        <div className="">
-          <div className="pt-6 pb-2">
-            <h1 className="text-4xl ">DinGullsmed</h1>
-            <p>Your Diamond in the rough</p>
-          </div>
-        </div>
-      </Link>
-
-      <div className="w-full md:max-w-lg flex flex-1 flex-row items-center pb-4 pt-2 ">
-        <form onSubmit={handleSearch} className="w-full">
-          <div className="flex flex-1 items-center gap-2 w-full px-4">
-            <div className="flex flex-1 items-center space-x-2 bg-white rounded-xl border px-2 py-2 md:max-w-sm">
-              <input
-                type="text"
-                name="searchProducts"
-                placeholder="Søk etter Produkt"
-                className="outline-none flex-1  "
-                onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
-              />
-              <SearchButton />
+      <div className="flex  w-[100%] justify-around">
+        <Link href="/">
+          <div className="">
+            <div className="pt-6 pb-2">
+              <h1 className="text-4xl ">DinGullsmed</h1>
+              <p>Your Diamond in the rough</p>
             </div>
           </div>
-        </form>
-        <div className="flex felx-1 gap-3">
-          <div>
-            {/* Bruker ikon */}
-            <UserIcon className="h-8 w-8" />
-          </div>
-          <div>
-            {/* Handlevogn ikon */}
-            <Link href="/shoppingCart" className="flex items-center">
-              <ShoppingCartIcon className="h-8 w-8" />
-              <div className="bg-orange-400 text-white flex items-center justify-center ml-[-14px] w-5 h-5 rounded-full mt-[-18px]">
-                {cart.length}
+        </Link>
+
+        <div className="w-full md:max-w-lg flex flex-1 flex-row items-center pb-4 pt-2 ">
+          <form onSubmit={handleSearch} className="w-full">
+            <div className="flex flex-1 items-center gap-2 w-full px-4">
+              <div className="flex flex-1 items-center space-x-2 bg-white rounded-xl border px-2 py-2 md:max-w-sm">
+                <input
+                  type="text"
+                  name="searchProducts"
+                  placeholder="Søk etter Produkt"
+                  className="outline-none flex-1  "
+                  onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+                />
+                <SearchButton />
               </div>
-            </Link>
+            </div>
+          </form>
+          <div className="flex felx-1 gap-3">
+            <div>
+              {/* Bruker ikon */}
+              <UserIcon className="h-8 w-8" />
+            </div>
+            <div>
+              {/* Handlevogn ikon */}
+              <Link href="/shoppingCart" className="flex items-center">
+                <ShoppingCartIcon className="h-8 w-8" />
+                <div className="bg-orange-400 text-white flex items-center justify-center ml-[-14px] w-5 h-5 rounded-full mt-[-18px]">
+                  {cart.length}
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-
       <div className="w-full flex items-center justify-center gap-16  ">
         {Object.keys(catalogue).map((kategori: string, index) => (
           <Link key={index} href={`/showcase/${kategori}`}>
